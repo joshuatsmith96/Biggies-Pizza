ec4442aa7e457d30b9ee5857c1cc060e6d5d2e8a
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2023-12-20 12:50:02 -0600

    Footer is complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Navigation from './components/Nav'
+import Footer from './components/Footer'
 
 //Pages
 import Home from './pages/Home/Home'
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/Biggies-Pizza/" element={ <Home /> } />
       </Routes>
+      <Footer />
     </div>
   )
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,12 @@
-function Footer() {
-    return (
+function Footer(){
+    return(
         <div className="Footer">
-
+            <div className="footer-info">
+                <a href="#">Menu</a>
+                <a href="#">Contact Us</a>
+                <a href="#">About</a>
+            </div>
+            <p>Joshuaswebsites 2023</p>
         </div>
     )
 }
