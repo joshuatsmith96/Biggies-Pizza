04297b02d81a366599802f2966b822e9577fecb5
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2023-12-20 11:01:35 -0600

    Home "Hero" Section Finished
    The Hero section on the Home page is complete

--- src/pages/Home/Home-Components/Hero.tsx
+++ src/pages/Home/Home-Components/Hero.tsx
@@ -1,6 +1,15 @@
 function Hero(){
     return(
         <div className="Hero">
+            <div className="Hero-Text">
+                <h1>Biggies</h1>
+                <p>Great Pizza</p>
+                <p>Good Vibes</p>
+                <div className="Hero-Buttons">
+                    <button>View Menu</button>
+                    <button>Book A Table</button>
+                </div>
+            </div>
             <div className="Hero-Overlay"></div>
         </div>
     )
