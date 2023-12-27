b4052a75b45e913f75bef71aacf7b7735cc4b27c
Josh Smith <jsmith@dailyprinting.local>
2023-12-27 12:14:12 -0600

    About Page Complete

--- src/pages/AboutUs/About.tsx
+++ src/pages/AboutUs/About.tsx
@@ -1,7 +1,8 @@
 import './About.css'
 
 //Images
-
+import img1 from '../../assets/Images/founder-image-example.jpg'
+import img2 from '../../assets/Images/building.jpg'
 
 function About() {
     return (
@@ -12,12 +13,28 @@ function About() {
             </div>
             <div id="about-origin">
                 <div id="image-overlap-container">
-                    <div id="image-overlap"></div>
+                    <div id="image-overlap">
+                        <img src={img1} alt="Founder" />
+                        <img src={img2} alt="Building" />
+                    </div>
                 </div>
-                <div id="content">
+                <div id="about-content">
                     <h1>Origin Story</h1>
                     <p>Biggies was started 85 years ago by John Smith, A WW2 veteran. After the war, John went to culinary school and fell in love with cooking. After many trips to Italy, John developed his world-famous crust and sauce recipes that are used at Biggies today, and opened up shop on Main Street in a small building.</p>
                 </div>
+            </div>
+            <div className="about-mission">
+                <h1>Our Mission</h1>
+                <p>Our goal is to provide amazing service, great food, and fond memories. We will always treat the customer with respect, regardless of the situation.</p>
+            </div>
+            <div className="about-promise">
+                <h1>Our Promise To You</h1>
+                <ul>
+                    <li>We <span className='Bold Italic'>promise</span> that if an order is wrong, we will fix it no questions asked.</li>
+                    <li>We <span className='Bold Italic'>promise</span> that all customers will be treated with respect.</li>
+                    <li>We <span className='Bold Italic'>promise</span> that all are welcome.</li>
+                </ul>
+                <p>The world is tough, so lets all gets together and eat some great pizza.</p>
             </div>
         </div>
     )
